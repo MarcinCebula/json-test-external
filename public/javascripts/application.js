63b62c5a4b4102407093b26df6ff60bacd16cf8f
Marcin <MarcinRKL@gmail.com>
2010-10-05 11:43:43 -0400

    changed url

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,8 +2,8 @@ jQuery(document).ready(function($) {
 	
 	
 	$.ajax({
-	  // url: "http://localhost:3000/cars.json",
-		url: "http://bedf.dyndns.info:9911/api?begin=l-5m&end=l&id=0,1&step=60&form=json",
+	  url: "http://radiant-flower-39.heroku.com/cars.json",
+		//url: "http://bedf.dyndns.info:9911/api?begin=l-5m&end=l&id=0,1&step=60&form=json",
 	  data: "GET",
 		dataType: "JSON",
 	  success: function() {

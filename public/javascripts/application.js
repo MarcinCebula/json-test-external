639c3e1d24a716939820219f2dc97c0b7bc0987e
Marcin <MarcinRKL@gmail.com>
2010-10-05 12:07:31 -0400

    added popup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,11 +3,10 @@ jQuery(document).ready(function($) {
 	
 	$.ajax({
 	  url: "http://radiant-flower-39.heroku.com/cars.json",
-		//url: "http://bedf.dyndns.info:9911/api?begin=l-5m&end=l&id=0,1&step=60&form=json",
 	  data: "GET",
 		dataType: "JSON",
-	  success: function() {
-			console.log("success")
+	  success: function(data) {
+			alert(data)
 	}
 	});
 	
